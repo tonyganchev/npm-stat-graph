38480c5f61fc3d47f7626a5520fd55eb8557760f
Tony Ganchev <tony.ganchev@gmail.com>
2026-03-22 14:39:13 +0200

    feat: set 'react' as the default package name

--- src/utils.ts
+++ src/utils.ts
@@ -8,7 +8,7 @@
 
 import { PackageConfig } from './types';
 
-export const defaultPackage = '@tony.ganchev/eslint-plugin-header';
+export const defaultPackage = 'react';
 
 export const packageColors = [
     '#f472b6', // Pink
