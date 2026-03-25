da4271cfa1090399e945071649144ee61f0bd3db
Tony Ganchev <tony.ganchev@gmail.com>
2026-03-25 13:21:24 +0200

    feat: sunday is last

--- src/utils.ts
+++ src/utils.ts
@@ -21,11 +21,11 @@ export const packageColors = [
 ];
 
 export const daysOfWeek = [
-    { label: 'Sun', value: 0 },
     { label: 'Mon', value: 1 },
     { label: 'Tue', value: 2 },
     { label: 'Wed', value: 3 },
     { label: 'Thu', value: 4 },
     { label: 'Fri', value: 5 },
     { label: 'Sat', value: 6 },
+    { label: 'Sun', value: 0 },
 ];
