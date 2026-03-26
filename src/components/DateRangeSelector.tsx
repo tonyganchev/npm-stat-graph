0ecd6f79252f9fe4d40d671a82bcbd8e7b495812
Tony Ganchev <tony.ganchev@gmail.com>
2026-03-26 17:30:26 +0200

    fixup! fixup! feat: ignore today for quick filters

--- src/components/DateRangeSelector.tsx
+++ src/components/DateRangeSelector.tsx
@@ -6,7 +6,7 @@
  * LICENSE.md file in the root directory of this source tree.
  */
 
-import { addDays, addMonths, format, startOfDay } from 'date-fns';
+import { addMonths, format, startOfDay } from 'date-fns';
 import { FC } from 'react';
 
 import { DateRangeType } from '../types';
